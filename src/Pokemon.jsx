35a1d9d283c9da6d6d900828c0787e161c16eb08
rohansharma99 <rohansharmaji9411@gmail.com>
2025-11-16 10:37:52 +0530

    commit

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -8,7 +8,7 @@ export const Pokemon = () => {
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
 
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=50";
 
   
 
